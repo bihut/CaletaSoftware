83edc7ff117c9e38165656f2ab227bc90bfd8bd2
bihut <angelrzafra@gmail.com>
2021-04-16 12:09:26 +0200

    ab

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
 import sys
+import time
 
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import QTime, QTimer
 
 from CaletaOAKD import CaletaAPI
 from mainUI import Ui_MainWindow
-caleta = None
-pipeline = None
-
+#caleta = None
+#pipeline = None
+#s = None
+s = None
 def handleCamera():
     if ui.enableCamera.isChecked():
         ui.enableCamera.setText("Stop Camera")
@@ -17,31 +21,54 @@ def handleCamera():
         ui.enableCamera.setIcon(QtGui.QIcon('imgs/play.png'))
         caleta.switchOffCamera()
 
+def startCounting():
+    global s
+    s = 0
+    timer.start(1000)
+
+def stopCounting():
+    timer.stop()
+    ui.watchcounter.display("00:00:00")
 
 def handleRecording():
     if ui.startRecording.isChecked():
+        startCounting()
+
         ui.startRecording.setText("Stop Recording")
         ui.startRecording.setIcon(QtGui.QIcon('imgs/record.png'))
         caleta.startRecording()
+
     else:
+        stopCounting()
         ui.startRecording.setText("Start Recording")
         ui.startRecording.setIcon(QtGui.QIcon())
-
         caleta.stopRecording()
 
+def LCDEvent():
+    global s
+    s += 1
+    ui.watchcounter.display(time.strftime('%H:%M:%S', time.gmtime(s)))
+    #ui.watchcounter.display(s)
 
-def uploadVideo():
-    caleta.stopCamera()
 
 if __name__ == "__main__":
+
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(window)
     ui.enableCamera.clicked.connect(handleCamera)
     ui.startRecording.clicked.connect(handleRecording)
-
+    _seconds = 0
     ui.enableCamera.setIcon(QtGui.QIcon('imgs/play.png'))
+    ui.watchcounter.setNumDigits(8)
+
+    #-----
+    s = 0
+    timer = QTimer()
+    timer.timeout.connect(LCDEvent)
+    ui.watchcounter.display("00:00:00")
+    #-----
 
 
     window.show()
